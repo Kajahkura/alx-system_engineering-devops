fed5de702bee362edaa42d7672acca5878c144f4
Kajahkura <obedinya4@gmail.com>
2023-12-25 05:18:06 -0800

    Configuration and Management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #Instal python flask from pip3
 
-package { 'flask' :
-ensure          => 'installed'
-install_options => ['-I', '2.1.0'],
-provider        => 'pip3',
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
